72d1d944ea1744dd1690970f07e12a60837bac31
Hans Raaf <hara@oderwat.de>
2021-10-02 17:36:53 +0200

    Reworked into a Guess The Number example

--- Makefile
+++ Makefile
@@ -1,7 +1,15 @@
-build:
+web/app.wasm: main.go
 	GOARCH=wasm GOOS=js go build -o web/app.wasm
-	go build
 
-run: build
+app: main.go
+	go build -o app
+
+run: app web/app.wasm
+	./app
+
+watch:
 	sleep 2 && open http://127.0.0.1:8000 &
-	./go-guess-the-number-app
+	ulimit -n 1000 # increasing watchable files
+	# I use clearing the scrollback buffer here so it is easier to
+	# relate to errors for the last compiled version
+	reflex -s -r '.*\.go' -- sh -c "printf '\033[2J\033[3J\033[1;1H' && make run"
--- main.go
+++ main.go
@@ -1,56 +1,102 @@
 package main
 
 import (
-	"github.com/maxence-charriere/go-app/v9/pkg/app"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
-// hello is a component that displays a simple "Hello World!". A component is a
-// customizable, independent, and reusable UI element. It is created by
-// embedding app.Compo into a struct.
-type hello struct {
+type mainPage struct {
 	app.Compo
+
+	url *url.URL
 }
 
-// The Render method is where the component appearance is defined. Here, a
-// "Hello World!" is displayed as a heading.
-func (h *hello) Render() app.UI {
-	return app.H1().Text("Hello World!")
+type guessTheNumber struct {
+	app.Compo
+
+	myNumber int
+	message  string
+	guess    string
 }
 
-// The main function is the entry point where the app is configured and started.
-// It is executed in 2 different environments: A client (the web browser) and a
-// server.
+var _ app.Mounter = (*guessTheNumber)(nil) // Verify the implementation
+var _ app.Navigator = (*mainPage)(nil)     // Verify the implementation
+
+func (p *mainPage) OnNav(ctx app.Context) {
+	p.url = ctx.Page().URL()
+}
+
+func (c *mainPage) Render() app.UI {
+	return app.Body().Body(&guessTheNumber{})
+}
+
+func (g *guessTheNumber) OnMount(ctx app.Context) {
+	g.myNumber = rand.Intn(100) + 1
+	g.message = "I think of a number between 1 and 100!"
+	g.guess = ""
+}
+
+func (g *guessTheNumber) onEnter(call func(ctx app.Context, e app.Event)) app.EventHandler {
+	return func(ctx app.Context, e app.Event) {
+		app.Logf("KeyboardEvent: %v", e.Value.Get("code"))
+		if e.Value.Get("code").String() != "Enter" {
+			return
+		}
+		call(ctx, e)
+	}
+}
+
+func (g *guessTheNumber) guessEvent(ctx app.Context, e app.Event) {
+	v, err := strconv.Atoi(ctx.JSSrc().Get("value").String())
+	if err != nil {
+		v = 0
+	}
+	if v < g.myNumber {
+		g.message = "Your number is smaller!"
+	} else if v > g.myNumber {
+		g.message = "Your number is bigger!"
+	} else {
+		g.message = "Congratulations! You found my number!"
+	}
+	ctx.JSSrc().Call("select")
+}
+
+func (g *guessTheNumber) Render() app.UI {
+	return app.Div().Body(
+		app.H1().Body(
+			app.Text("Guess my Number!"),
+		),
+		app.P().Body(
+			app.P().Body(app.Text(g.message)),
+			app.Input().
+				Type("text").
+				Value(g.guess).
+				Placeholder("Your guess (1-100)?").
+				AutoFocus(true).
+				OnKeyup(g.onEnter(g.guessEvent)),
+		),
+	)
+}
+
 func main() {
-	// The first thing to do is to associate the hello component with a path.
-	//
-	// This is done by calling the Route() function,  which tells go-app what
-	// component to display for a given path, on both client and server-side.
-	app.Route("/", &hello{})
+	rand.Seed(time.Now().UnixNano()) // needs to be better for a real app
 
-	// Once the routes set up, the next thing to do is to either launch the app
-	// or the server that serves the app.
-	//
-	// When executed on the client-side, the RunWhenOnBrowser() function
-	// launches the app,  starting a loop that listens for app events and
-	// executes client instructions. Since it is a blocking call, the code below
-	// it will never be executed.
-	//
-	// When executed on the server-side, RunWhenOnBrowser() does nothing, which
-	// lets room for server implementation without the need for precompiling
-	// instructions.
+	// Components routing:
+	app.Route("/", &mainPage{})
+	//app.Route("/hello", &hello{})
+
 	app.RunWhenOnBrowser()
 
-	// Finally, launching the server that serves the app is done by using the Go
-	// standard HTTP package.
-	//
-	// The Handler is an HTTP handler that serves the client and all its
-	// required resources to make it work into a web browser. Here it is
-	// configured to handle requests with a path that starts with "/".
+	// HTTP routing:
 	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "An Hello World! example",
+		Name:        "Guess the Number",
+		Description: "A simple game!",
 	})
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
