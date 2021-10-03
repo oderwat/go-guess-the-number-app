c4589620d3230e814ace60c1dbe19d2cd7ded622
Hans Raaf <hara@oderwat.de>
2021-10-03 17:59:51 +0200

    Experimental LiveReload and updated the README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-guess-the-number-app
-A simple "guess the number" game for testing "go-app"
+A simple "guess the number" using the [go-app](https://github.com/maxence-charriere/go-app) framework!
+
+If you have [Reflex](https://github.com/cespare/reflex) installed, you can run it with semi LiveReload using `make watch`.
--- main.go
+++ main.go
@@ -30,26 +30,43 @@ type guessTheNumber struct {
 var _ app.Mounter = (*guessTheNumber)(nil) // Verify the implementation
 var _ app.Navigator = (*mainPage)(nil)     // Verify the implementation
 
-func (p *mainPage) OnNav(ctx app.Context) {
-	p.url = ctx.Page().URL()
+func (a *mainPage) OnNav(ctx app.Context) {
+	a.url = ctx.Page().URL()
+	//app.Logf(p.url.Fragment)
+	if a.url.Fragment == "autoupdate" {
+		app.Logf("auto update enabled")
+		a.autoUpdate = true
+	} else {
+		app.Logf("auto update disabled")
+		a.autoUpdate = false
+	}
 }
 
-func (c *mainPage) Render() app.UI {
+func (a *mainPage) Render() app.UI {
 	count := 1 // try 1000 if you like
 	body := make([]app.UI, 0, count+5)
 	body = append(body,
-		app.If(c.updateAvailable,
+		app.If(a.updateAvailable,
 			app.Button().
 				Text("Update!").
-				OnClick(c.onUpdateClick),
+				OnClick(a.onUpdateClick),
 			app.Hr()))
 	for i := 0; i < count; i++ {
 		body = append(body, &guessTheNumber{})
 	}
-	return app.Main().Body(append(body, app.Hr(), app.Text("Ends here!"))...)
+	var aumodeFragment, aumodeText string
+	if a.autoUpdate {
+		aumodeFragment = "#"
+		aumodeText = "Automatic updates"
+	} else {
+		aumodeFragment = "#autoupdate"
+		aumodeText = "Manual updates"
+	}
+	return app.Main().Body(append(body, app.Hr(),
+		app.A().Href(aumodeFragment).Text(aumodeText))...)
 }
 
-func (a *mainPage) onUpdateClick(ctx app.Context, e app.Event) {
+func (a *mainPage) onUpdateClick(ctx app.Context, _ app.Event) {
 	// Reloads the page to display the modifications.
 	ctx.Reload()
 }
@@ -58,9 +75,12 @@ func (a *mainPage) onUpdateClick(ctx app.Context, e app.Event) {
 // is updated in background.
 func (a *mainPage) OnAppUpdate(ctx app.Context) {
 	a.updateAvailable = ctx.AppUpdateAvailable() // Reports that an app update is available.
+	if a.updateAvailable && a.autoUpdate {
+		ctx.Reload()
+	}
 }
 
-func (g *guessTheNumber) OnMount(ctx app.Context) {
+func (g *guessTheNumber) OnMount(_ app.Context) {
 	g.gameInit()
 }
 
@@ -84,7 +104,7 @@ func (g *guessTheNumber) onEnter(call func(ctx app.Context, e app.Event)) app.Ev
 	}
 }
 
-func (g *guessTheNumber) guessEvent(ctx app.Context, e app.Event) {
+func (g *guessTheNumber) guessEvent(ctx app.Context, _ app.Event) {
 	guess := ctx.JSSrc().Get("value").String()
 	if guess == "Restart!" {
 		g.gameInit()
@@ -140,7 +160,10 @@ func main() {
 	// HTTP routing:
 	http.Handle("/", &app.Handler{
 		Name:        "Guess the Number",
-		Description: "A simple game!",
+		Description: "A simple game using go-app as framework!",
+		Scripts: []string{
+			"https://livejs.com/live.js", // Add simple live reloading
+		},
 	})
 
 	if err := http.ListenAndServe(":8000", nil); err != nil {
